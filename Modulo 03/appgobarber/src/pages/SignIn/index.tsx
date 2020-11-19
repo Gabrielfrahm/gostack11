52b7d581c7a57f398656326cdf23e67be3ef1941
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2020-11-18 21:16:49 -0300

    App iniciado

---
+++ Modulo 03/appgobarber/src/pages/SignIn/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+import LogoImg from '../../assets/logo.png';
+
+const SignIn: React.FC =()=> {
+  return (
+    <Container>
+      <Image  source={LogoImg}/>
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail"/>
+      <Input name="password" icon="lock" placeholder="Senha"/>
+      <Button onPress={() => {console.log('Dale')}}>Entrar</Button>
+    </Container>
+  );
+}
+
+export default SignIn;
